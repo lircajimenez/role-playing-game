1806ba970aaa016270c4f488711cca71119386e2
Lirca Jiménez <78028056+lircajimenez@users.noreply.github.com>
2022-03-05 08:35:57 -0500

    Housekeeping: break index.js into different files

---
+++ Character.js
@@ -0,0 +1,30 @@
+import { getDiceRollArray } from "./utils.js";
+
+//constructor function that will be the template for Characters
+function Character(data) {
+  Object.assign(this, data);
+
+  this.getDiceHTML = function (diceCount) {
+    return getDiceRollArray(diceCount)
+      .map((dice) => {
+        return `<div class="dice">${dice}</div>`;
+      })
+      .join("");
+  };
+
+  this.getCharacterHTML = function () {
+    const { elementID, name, avatar, health, diceCount } = this;
+    const diceHTML = this.getDiceHTML(diceCount);
+
+    return `
+          <div class="character-card">
+          <h4 class="name">${name}</h4>
+          <img class="avatar" src="${avatar}" />
+          <p class="health">health: <b> ${health} </b></p>
+          <div class="dice-container">${diceHTML}</div>
+          </div>
+      `;
+  };
+}
+
+export default Character;
--- index.js
+++ index.js
@@ -1,59 +1,11 @@
-const hero = {
-  elementID: "hero",
-  name: "Wizard",
-  avatar: "images/wizard.png",
-  health: 60,
-  diceCount: 3,
-};
-
-const monster = {
-  elementID: "monster",
-  name: "Orc",
-  avatar: "images/orc.png",
-  health: 10,
-  diceCount: 1,
-};
-
-function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(() => {
-    return Math.floor(Math.random() * 6) + 1;
-  });
-}
-
-//constructor function that will be the template for Characters
-function Character(data) {
-  Object.assign(this, data);
-
-  this.getDiceHTML = function (diceCount) {
-    return getDiceRollArray(diceCount)
-      .map((dice) => {
-        return `<div class="dice">${dice}</div>`;
-      })
-      .join("");
-  };
-
-  this.getCharacterHTML = function () {
-    const { elementID, name, avatar, health, diceCount } = this;
-    const diceHTML = this.getDiceHTML(diceCount);
-
-    return `
-        <div class="character-card">
-        <h4 class="name">${name}</h4>
-        <img class="avatar" src="${avatar}" />
-        <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container">${diceHTML}</div>
-        </div>
-    `;
-  };
-}
-
-const wizard = new Character(hero);
-const orc = new Character(monster);
+import characterData from "./data.js";
+import Character from "./Character.js";
 
 function render() {
-  document.getElementById(wizard.elementID).innerHTML =
-    wizard.getCharacterHTML();
-  document.getElementById(orc.elementID).innerHTML = orc.getCharacterHTML();
+  document.getElementById("hero").innerHTML = wizard.getCharacterHTML();
+  document.getElementById("monster").innerHTML = orc.getCharacterHTML();
 }
 
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
 render();
